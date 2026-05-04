1edd6e23ee0544d0c0f2615f9643405984df0611
TEBANCIO <dami.caceres@duocuc.cl>
2026-05-04 18:09:02 -0400

    conexion carrito

---
+++ src/main/java/com/example/ventas/config/WebCarritoConfig.java
@@ -0,0 +1,15 @@
+package com.example.ventas.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebCarritoConfig {
+
+    @Bean(name = "carritoWebClient")
+    public WebClient webClient(){
+         return WebClient.builder().baseUrl("http://localhost:9093/api/v1/carrito").build();
+     }
+}
