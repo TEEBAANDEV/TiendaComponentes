51bffc5a093fe879c122769c6348c35355e97c7a
TEBANCIO <dami.caceres@duocuc.cl>
2026-05-04 00:05:15 -0400

    Añadi un nuevo requestMatcher

--- src/main/java/com/example/ms_users/security/config/SecurityConfig.java
+++ src/main/java/com/example/ms_users/security/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/api/v1/users/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
