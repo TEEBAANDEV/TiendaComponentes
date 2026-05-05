5a54828d95001e3bf6a0303e3056b597dcf23bdb
TEBANCIO <dami.caceres@duocuc.cl>
2026-05-04 22:15:11 -0400

    Actualizacion - Agregados

--- src/main/java/com/example/ventas/service/VentaService.java
+++ src/main/java/com/example/ventas/service/VentaService.java
@@ -1,21 +1,52 @@
 package com.example.ventas.service;
 
 
+import com.example.ventas.client.CarritoClient;
+import com.example.ventas.client.ProductoClient;
+import com.example.ventas.model.CarritoDTO;
 import com.example.ventas.model.Venta;
 import com.example.ventas.respository.VentaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class VentaService {
 
     @Autowired
-    private VentaRepository repository;
+    private final VentaRepository repository;
+    @Autowired
+    private final CarritoClient carritoClient;
+    @Autowired
+    private final ProductoClient productoClient;
 
-    public Venta agregarVenta(Venta nuevaVenta) {
-        return repository.save(nuevaVenta);
+    public Mono<Venta> procesarVenta(Long idUsuario){
+        return carritoClient.obtenerCarritoPorUsuario(idUsuario)
+                .flatMap((List<CarritoDTO> items) ->{
+                    if(items == null || items.isEmpty()){
+                        return Mono.error(new RuntimeException("El carrito esta vacio, no se puede procesar la venta"));
+                    }
+                    return Flux.fromIterable(items)
+                            .flatMap(item -> productoClient.obtenerProducto(item.getIdProducto())
+                                    .map(productoDTO -> productoDTO.getPrecio() * item.getCantidad())
+                            )
+                            .reduce(0.0, Double::sum)
+                            .map(totalCalculado -> {
+                               Venta nuevaVenta = Venta.builder()
+                                       .idUsuario(idUsuario)
+                                       .total(totalCalculado)
+                                       .fecha(LocalDateTime.now())
+                                       .estado("PAGADA")
+                                       .build();
+                               return repository.save(nuevaVenta);
+                            });
+                });
     }
 
     public List<Venta> obtenerVentas(){
@@ -25,4 +56,6 @@ public class VentaService {
     public void eliminarVenta(Long id){
         repository.deleteById(id);
     }
+
+
 }
