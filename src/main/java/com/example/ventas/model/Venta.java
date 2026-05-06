6ad6850a7febb43b82d78f7c670acb896684e9ce
ItsariMoreno <it.moreno@duocuc.cl>
2026-05-06 01:06:06 -0400

    Actualizacion - conexion con carrito, reporte(soon), inventario y usuario para descontar en base de datos

--- src/main/java/com/example/ventas/model/Venta.java
+++ src/main/java/com/example/ventas/model/Venta.java
@@ -2,8 +2,10 @@ package com.example.ventas.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
@@ -11,6 +13,8 @@ import java.time.LocalDateTime;
 @Data
 @Table(name = "ventas")
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Venta {
 
     @Id
