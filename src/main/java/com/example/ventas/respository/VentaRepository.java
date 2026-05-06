5b3b98224e538b8f8d7e7760b79e7463a3017ddd
ItsariMoreno <it.moreno@duocuc.cl>
2026-05-05 21:18:28 -0400

    Actualizacion

--- src/main/java/com/example/ventas/respository/VentaRepository.java
+++ src/main/java/com/example/ventas/respository/VentaRepository.java
@@ -3,5 +3,8 @@ package com.example.ventas.respository;
 import com.example.ventas.model.Venta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VentaRepository extends JpaRepository<Venta,Long> {
+    List<Venta> findByIdUsuario(Long idUsuario);
 }
