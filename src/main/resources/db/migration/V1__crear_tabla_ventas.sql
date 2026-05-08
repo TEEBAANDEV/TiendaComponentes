b2ba96552a3247266e8ed35e0c113fa169c92e51
ItsariMoreno <it.moreno@duocuc.cl>
2026-05-07 22:39:07 -0400

    Actualizacion - creacion tabla para detalles de venta por actualizacion de carrito

--- src/main/resources/db/migration/V1__crear_tabla_ventas.sql
+++ src/main/resources/db/migration/V1__crear_tabla_ventas.sql
@@ -1,7 +1,17 @@
-CREATE TABLE ventas(
+CREATE TABLE ventas (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     id_usuario BIGINT NOT NULL,
     total DOUBLE NOT NULL,
     fecha DATETIME NOT NULL,
     estado VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE venta_detalles (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    venta_id BIGINT NOT NULL,
+    nombre_producto VARCHAR(100) NOT NULL,
+    descripcion VARCHAR(255),
+    cantidad INT NOT NULL,
+    precio_unitario DOUBLE,
+    CONSTRAINT fk_venta FOREIGN KEY (venta_id) REFERENCES ventas(id) ON DELETE CASCADE
 );
