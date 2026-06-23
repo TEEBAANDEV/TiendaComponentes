5a3e0f52aa52096fc61dd96ffa3dd86871a9e5c2
ItsariMoreno <it.moreno@duocuc.cl>
2026-06-22 21:05:58 -0400

    Se probaron los test de distintos microservicios :D

--- src/test/java/com/example/componentesv2/Componentesv2ApplicationTests.java
+++ src/test/java/com/example/componentesv2/Componentesv2ApplicationTests.java
@@ -16,7 +16,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class ProductoServiceTest {
+class Componentesv2ApplicationTests {
 
     @Mock
     private ProductoRepository repository;
